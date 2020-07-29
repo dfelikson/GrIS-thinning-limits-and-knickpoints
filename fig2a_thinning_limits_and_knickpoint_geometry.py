7389e15f21583a0760a39723fc3b5c47b6901df3
dfelikson <>
2020-07-28 21:59:10 -0400

    various fixes to fig2a script

--- fig2a_thinning_limits_and_knickpoint_geometry.py
+++ fig2a_thinning_limits_and_knickpoint_geometry.py
@@ -5,17 +5,14 @@ import glob
 
 from netCDF4 import Dataset
 
-from moving_average import moving_average
-
 import utils
 from matplotlib import pyplot as plt
 
 # Setup
 netcdf_dir = 'netcdfs'
-flowlines = ['flowline03', 'flowline04', 'flowline05', 'flowline06', 'flowline07', 'flowline08']
 
-basins_gtl = [1.2, 1.3, 2.2, 3.1, 3.2, 3.3, 4.1, 4.3, 5.0, 6.1, 7.2]
-basins_mtn = [1.1, 1.4, 2.1, 4.2, 6.2, 7.1, 8.1, 8.2]
+basins_mtn = [1.2, 1.3, 2.2, 3.1, 3.2, 3.3, 4.1, 4.3, 5.0, 6.1, 7.2]
+basins_gtl = [1.1, 1.4, 2.1, 4.2, 6.2, 7.1, 8.1, 8.2]
 
 # Processing
 basins_2categories = dict()
@@ -29,59 +26,41 @@ for ncfile in glob.glob(netcdf_dir + '/glacier????.nc'):
    glacier = ncfile.split('/')[-1].split('.')[0]
    diffs[glacier] = dict()
    
-   for flowline in flowlines:
-      if flowline in ds.groups.keys():
-         d = ds[flowline]['d'][:]
+   flowline_groups, iteration_list = utils.get_flowline_groups(ds)
+   for flowline_group, iteration in zip(flowline_groups, iteration_list):
+      x = flowline_group['x'][:]
+      y = flowline_group['y'][:]
+      d = flowline_group['d'][:]
 
-         # Get flowline length
-         #parts = flowline.split('_')
-         #if len(parts) == 2: ln = utils.getFlowlineLength(glacier, flowline.replace('flowline',''))
-         #else:               ln = utils.getFlowlineLength(glacier, flowline.replace('flowline',''), iteration)
-         flowline_length = utils.getFlowlineLength(glacier, flowline.replace('flowline',''))
+      # Get flowline length
+      flowline_length = utils.getFlowlineLength(glacier, flowline_group.name.replace('flowline',''), iterID=iteration)
 
-         Pe1 = ds[flowline]['Pe']['AERO']['nominal'][:]
-         Pe2 = ds[flowline]['Pe']['GIMP']['nominal'][:]
-         lastvalid = np.max( ~np.isnan(Pe1) )
-         Pe = np.hstack( (Pe1[:lastvalid], Pe2[lastvalid:]) )
-         geoid = ds[flowline]['geometry']['geoid']['h'][:]
+      Pe1 = flowline_group['Pe']['AERO']['nominal'][:]
+      Pe2 = flowline_group['Pe']['GIMP']['nominal'][:]
+      lastvalid = np.max( ~np.isnan(Pe1) )
+      Pe = np.hstack( (Pe1[:lastvalid], Pe2[lastvalid:]) )
+      geoid = flowline_group['geometry']['geoid']['h'][:]
 
-         dh = ds[flowline]['dh']['AERO-Arctic']['nominal']['dh'][:]
-         dh1 = ds[flowline]['dh']['GIMP-Arctic']['nominal']['dh'][:]
+      ID = glacier + '_' + flowline_group.name.replace('flowline','')
+      _, _, PeD = utils.get_Pe3(ID, x, y, d, Pe1, Pe2)
 
-         dhavg, dhstddev = moving_average(d, dh, 20)
-         dhmax = np.nanmin(dhavg)
-         dhpercent = 100.0 * (dhavg/dhmax)
+      # Bed = sea level
+      bed = flowline_group['geometry']['bed']['BedMachine']['nominal']['h_mvavg'][:]
+      valid = ~np.isnan(bed)
+      idx = np.where(bed[valid] > geoid[valid])[0]
+      if len(idx) > 0:
+         d_bed_sealevel = d[valid][np.min(idx)]
+      else:
+         d_bed_sealevel = flowline_length
 
-         dhthinning = np.where(dhavg < 0, dhavg, np.nan)
-         dhcumtotal = np.nansum(dhthinning)
-         dhcumsum = np.cumsum(np.where(~np.isnan(dhthinning), dhthinning, 0))
-         dhcumpercent = 100.0 * (dhcumsum / dhcumtotal)
-
-         # Find where unit volume loss is 93%
-         dhcumpercentThreshold = 93.
-         idx = np.where(dhcumpercent >= dhcumpercentThreshold)[0]
-         if len(idx) > 0:
-            d_threshold = d[np.min(idx)]
-         else:
-            d_threshold = flowline_length
-
-         # Bed = sea level
-         bed = ds[flowline]['geometry']['bed']['BedMachine']['nominal']['h_mvavg'][:]
-         valid = ~np.isnan(bed)
-         idx = np.where(bed[valid] > geoid[valid])[0]
-         if len(idx) > 0:
-            d_bed_sealevel = d[valid][np.min(idx)]
-         else:
-            d_bed_sealevel = flowline_length
-
-         # Collect all data
-         diffs[glacier][flowline] = (d_threshold-d_bed_sealevel)/1000.
+      # Collect all data
+      diffs[glacier][flowline_group.name + '_' + iteration] = (PeD - d_bed_sealevel)/1000.
 
    ds.close()
 
-   if (np.abs(basin - basins_mtn) < 0.01).any():
+   if basin in basins_mtn:
       basins_2categories[glacier] = 'mountainous'
-   elif (np.abs(basin - basins_gtl) < 0.01).any():
+   elif basin in basins_gtl:
       basins_2categories[glacier] = 'gentle'
 
 utils.violin_plot(diffs, basins_2categories, ['gentle', 'mountainous'], 'fig2a.pdf') #, highlight=highlight)
